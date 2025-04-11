247b7c10c57cdea49c7d43a3570ab824fc11153d
MikaM-droid <mikamonsma@gmail.com>
2025-04-11 10:24:54 +0200

    Comments

--- Hfdst5/mikamonsma-503-rgb-led-temp-vocht/mikamonsma-503-rgb-led-temp-vocht.ino
+++ Hfdst5/mikamonsma-503-rgb-led-temp-vocht/mikamonsma-503-rgb-led-temp-vocht.ino
@@ -1,22 +1,27 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-  #define LED_R 9
+  // Define the pins for the LED and the button
+  #define LED_R 9 
   #define LED_G 10
   #define LED_B 11
   #define LMPIN A2
 
+  // Define the type of sensor and the pin for the sensor
   #define DHTTYPE DHT11
   #define DHTPIN 4
 
+  // Define the object for the sensor
   DHT dht = DHT(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialize the sensor
   dht.begin();
 
+  // Initialize the serial communication
   Serial.begin(9600);
 
+  // Set the pins for the LED as outputs
   pinMode(LED_R, OUTPUT) ;
   pinMode(LED_G, OUTPUT) ;
   pinMode(LED_B, OUTPUT) ;
@@ -24,31 +29,38 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read the temperature from the sensor
+  // Use float to store the temperature making sure it's a decimal
   float temperatuurtje = dht.readTemperature();
 
+  // If the temperature is not a number, print an error message
   if(isnan(temperatuurtje)) {
     Serial.println("Failed bozo");
     return;
   }
 
+  // Print the temperature
   Serial.println(String(temperatuurtje) + " graden C");
 
+  // If the temperature is less than 15, turn on the red LED
   if (temperatuurtje < 15) {
     analogWrite(LED_R, 0);
     analogWrite(LED_G, 0);
     analogWrite(LED_B, 255);
     }
+  // If the temperature is between 15 and 23, turn on the green LED
   else if (temperatuurtje >= 15 && temperatuurtje < 23) {
     analogWrite(LED_R, 0);
     analogWrite(LED_G, 255);
     analogWrite(LED_B, 0);    
   }
+  // If the temperature is greater than 23, turn on the blue LED
   else {
     analogWrite(LED_R, 255);
     analogWrite(LED_G, 0);
     analogWrite(LED_B, 0);    
   }
 
+  // Wait 2 seconds before reading the temperature again
   delay(2000);
 }
--- Hfdst5/mikamonsma-504-pot-4x-led/mikamonsma-504-pot-4x-led.ino
+++ Hfdst5/mikamonsma-504-pot-4x-led/mikamonsma-504-pot-4x-led.ino
@@ -1,16 +1,17 @@
+// Potentiometer and 4 LEDs
+// Define the pins for the LED and the potentiometer
 #define LED1 10
 #define LED2 11
 #define LED3 12
 #define LED4 13
 
+// Define the pin for the potentiometer
 #define POT_PIN A0
 
-
-
 void setup() {
-  // put your setup code here, to run once:
+  // Set the pin for the potentiometer as an input
   pinMode(POT_PIN, INPUT);
-
+  // Set the pins for the LEDs as outputs
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
@@ -21,37 +22,44 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // Read the potentiometer
+  // Analogread is used to output Pulse Width Modulation (PWM) values
+  // Which can be used to control the brightness of the LEDs
   int knopje = analogRead(POT_PIN);
   delay(500);
 
   Serial.println(knopje);
 
+  // If the potentiometer is less than 128, turn on all the LEDs
   if (knopje < 128) {
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, HIGH);
 
+  // If the potentiometer is between 128 and 384, turn on the first LED and turn off the other three
   } else if (knopje > 128 && knopje < 384) {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, HIGH);
 
+  // If the potentiometer is between 384 and 640, turn on the first two LEDs and turn off the other two
   } else if (knopje > 384 && knopje < 640) {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, HIGH);
 
+  // If the potentiometer is between 640 and 896, turn on the first two LEDs and turn off the other two
   } else if (knopje > 640 && knopje < 896) {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, HIGH);
-    
+
+  // If the potentiometer is greater than 896, turn off all the LEDs
   } else if (knopje > 896) {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
