62d2bbb81289df8a29fd45b3edc69aa03cc69692
Mika <mikamonsma@gmail.com>
2025-05-19 11:18:37 +0200

    rtrdt

---
+++ Hfdst10/Car2/Car2.ino
@@ -0,0 +1,328 @@
+//---------------------------------------------------
+// Arduino car
+
+#include <SoftwareSerial.h>
+
+//---------------------------------------------------
+// Pin defining
+
+// Motor control pins
+#define ENA 3
+#define IN1 4
+#define IN2 5
+
+#define ENB 10
+#define IN3 9
+#define IN4 8
+
+// Bluetooth
+#define BT_RX 2
+#define BT_TX 7
+SoftwareSerial BT(BT_RX, BT_TX);
+
+// Buzzer
+#define PIEP 12
+
+// Lijnsensoren
+#define LINE_LEFT A0
+#define LINE_CENTER A1
+#define LINE_RIGHT A2
+
+// Object sensors
+#define OBJECT_LEFT A3
+#define OBJECT_RIGHT A4
+
+// Ultrasonic
+#define TRIG_PIN 6
+#define ECHO_PIN 0 // D0
+
+// Bluetooth Commands
+#define FORWARD 'F'
+#define BACKWARD 'B'
+#define LEFT 'L'
+#define RIGHT 'R'
+#define STOP 'S'
+#define BEEP 'Y'
+#define SPEED 'V'
+
+//---------------------------------------------------
+// Speed values
+
+int currentSpeed = 150;
+int minSpeed = 80;
+int maxSpeed = 255;
+
+//---------------------------------------------------
+// Pin setup, to make sure the Input and Output
+// are correctly noted.
+
+void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(PIEP, OUTPUT);
+  digitalWrite(PIEP, HIGH);
+
+  pinMode(LINE_LEFT, INPUT);
+  pinMode(LINE_CENTER, INPUT);
+  pinMode(LINE_RIGHT, INPUT);
+
+  pinMode(OBJECT_LEFT, INPUT);
+  pinMode(OBJECT_RIGHT, INPUT);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  BT.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Arduino Bluetooth Car Ready!");
+  delay(100);
+}
+
+//---------------------------------------------------
+// Car forward and backward functions
+
+// Movement
+void forward(int speed) {
+  analogWrite(ENA, speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  analogWrite(ENB, speed);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  Serial.print("Moving forward, speed: ");
+  Serial.println(speed);
+}
+
+//---------------------------------------------------
+
+void backward(int speed) {
+  analogWrite(ENA, speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  analogWrite(ENB, speed);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  Serial.print("Moving backward, speed: ");
+  Serial.println(speed);
+}
+
+//---------------------------------------------------
+// Car turning functions
+
+void left(int speed) {
+  analogWrite(ENA, speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  analogWrite(ENB, speed);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  Serial.print("Turning left, speed: ");
+  Serial.println(speed);
+}
+
+//---------------------------------------------------
+
+void right(int speed) {
+  analogWrite(ENA, speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  analogWrite(ENB, speed);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  Serial.print("Turning right, speed: ");
+  Serial.println(speed);
+}
+
+//---------------------------------------------------
+// Stop function
+
+void stop() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
+  Serial.println("Stopped");
+}
+
+//---------------------------------------------------
+// Beeping function
+
+void beep() {
+  Serial.println("Beep!");
+  digitalWrite(PIEP, LOW);
+  delay(100);
+  digitalWrite(PIEP, HIGH);
+  delay(100);
+}
+
+//---------------------------------------------------
+// Speed control
+
+void processSpeedData() {
+  Serial.println("Speed command detected!");
+  delay(50);
+  if (BT.available()) {
+    int speedByte = BT.read();
+    Serial.print("Raw speed data: ");
+    Serial.print(speedByte);
+    Serial.print(" (");
+    Serial.write(speedByte);
+    Serial.println(")");
+
+    if (speedByte >= '0' && speedByte <= '9') {
+      int speedValue = speedByte - '0';
+      setSpeedFromValue(speedValue);
+      return;
+    }
+
+    if (speedByte >= 1 && speedByte <= 10) {
+      setSpeedFromValue(speedByte);
+      return;
+    }
+
+    if (speedByte >= 0 && speedByte <= 255) {
+      currentSpeed = constrain(speedByte, minSpeed, maxSpeed);
+      Serial.print("Direct speed set to: ");
+      Serial.println(currentSpeed);
+    }
+  }
+}
+
+//---------------------------------------------------
+
+void setSpeedFromValue(int value) {
+  int speedValue = map(value, 0, 10, minSpeed, maxSpeed);
+  currentSpeed = speedValue;
+  Serial.print("Mapped speed value ");
+  Serial.print(value);
+  Serial.print(" to motor speed: ");
+  Serial.println(currentSpeed);
+}
+
+//---------------------------------------------------
+// Ultrasonic distance function
+
+long getDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  return duration * 0.034 / 2;
+}
+
+//---------------------------------------------------
+
+// Autonomous logic
+void autonomousMode() {
+  int distance = getDistance();
+  bool leftLine = digitalRead(LINE_LEFT);
+  bool centerLine = digitalRead(LINE_CENTER);
+  bool rightLine = digitalRead(LINE_RIGHT);
+  bool objLeft = digitalRead(OBJECT_LEFT);
+  bool objRight = digitalRead(OBJECT_RIGHT);
+
+  Serial.print("Afstand: "); Serial.println(distance);
+  Serial.print("Lijnsensoren: L="); Serial.print(leftLine);
+  Serial.print(" C="); Serial.print(centerLine);
+  Serial.print(" R="); Serial.println(rightLine);
+
+  if (distance < 15 || objLeft == LOW || objRight == LOW) {
+    stop();
+    beep();
+    delay(400);
+    backward(currentSpeed);
+    delay(400);
+    right(currentSpeed);
+    delay(500);
+    return;
+  }
+
+  if (leftLine == LOW && centerLine == HIGH && rightLine == LOW) {
+    forward(currentSpeed);
+  } else if (leftLine == HIGH && centerLine == LOW) {
+    left(currentSpeed);
+  } else if (rightLine == HIGH && centerLine == LOW) {
+    right(currentSpeed);
+  } else {
+    stop();
+  }
+}
+
+//---------------------------------------------------
+
+// Command handler
+void executeCommand(char command) {
+  Serial.print("Command received: ");
+  Serial.println(command);
+
+  switch (command) {
+    case FORWARD: forward(currentSpeed); break;
+    case BACKWARD: backward(currentSpeed); break;
+    case LEFT: left(currentSpeed); break;
+    case RIGHT: right(currentSpeed); break;
+    case STOP: stop(); break;
+    case BEEP: beep(); break;
+    case SPEED: processSpeedData(); break;
+    case 'A':
+      Serial.println("Autonome modus gestart...");
+      while (true) {
+        autonomousMode();
+        delay(50);
+        if (BT.available()) {
+          char stopCommand = BT.read();
+          if (stopCommand == 'S') {
+            stop();
+            Serial.println("Autonome modus gestopt.");
+            break;
+          }
+        }
+      }
+      break;
+    case '0'...'9':
+      setSpeedFromValue(command - '0');
+      break;
+    default:
+      Serial.print("Unknown command: ");
+      Serial.write(command);
+      Serial.print(" (");
+      Serial.print((int)command);
+      Serial.println(")");
+      break;
+  }
+}
+
+//---------------------------------------------------
+
+// Main loop
+void loop() {
+  while (BT.available()) {
+    char command = BT.read();
+    Serial.print("Received: ");
+    Serial.write(command);
+    Serial.print(" (");
+    Serial.print((int)command);
+    Serial.println(")");
+    executeCommand(command);
+    delay(10);
+  }
+}
